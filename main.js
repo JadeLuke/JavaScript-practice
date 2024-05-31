5142bf14a479024652b95bbd999c65036b3fa0cb
JadeLuke <jadeluke.booysen@gmail.com>
2024-05-31 16:32:12 +0200

    added calc

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-let num1 = 12;
-let num2 = 2;
-let answer = num1 * num2;
-console.log('your answer is ' +  12 );
+let num1 = prompt('Enter first number', 0);
+let op = prompt('Enter operator');
+let num2 = prompt('second number', 0);
+let answer = num1+num2
+if (op==='*')
+{console.log('Your answer is ' + (num1*num2));
+} 
+else if(op==='-')
+{
+    console.log('Your answer is ' + (num1-num2));
+}
+else if(op==='/')
+{
+    console.log('Your answer is '+ (num1/num2));
+}
+else if(op==='+')
+{
+    //  console.log('Your answer is ' + (num1+num2));
+     console.log('Answer is' + answer)
+}
+else {
+    console.log('Invalid operator')
+}
