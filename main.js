c9be338103a8606c32c15cf1b3a16eea50baae18
JadeLuke <jadeluke.booysen@gmail.com>
2024-06-03 11:27:28 +0200

    added name prompt and result message

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
+let name = prompt('Enter your name')
 let age = prompt('Enter your age');
 let citizenship = prompt('Enter Citizentship')
 if (age >= 18 && citizenship==='South African') 
 { 
-    alert('You can vote')
+    alert('Hi ' + name + ', you can vote')
 }
 
 else{
-    alert("You can't vote")
+    alert('Hi ' + name + ", you can't vote")
 }
 
