d185d812e07a11b38ce823cfa6e20db78c428a98
JadeLuke <jadeluke.booysen@gmail.com>
2024-06-03 10:49:13 +0200

    removed console log

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ let age = prompt('Enter your age');
 let citizenship = prompt('Enter Citizentship')
 if (age >= 18 && citizenship==='South African') 
 { 
-    console.log('Can vote')
     alert('You can vote')
 }
 
