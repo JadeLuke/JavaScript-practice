c13d892cd03366a41ee3f0a5a3fd8286ebecf07a
JadeLuke <jadeluke.booysen@gmail.com>
2024-06-03 15:25:53 +0200

    added murendeni code

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-let name = prompt('Enter your name')
-let age = prompt('Enter your age');
-let citizenship = prompt('Enter Citizentship')
-if (age >= 18 && citizenship==='South African') 
-{ 
-    alert('Hi ' + name + ', you can vote')
-}
+// let name = prompt('Enter your name')
+// let age = prompt('Enter your age');
+// let citizenship = prompt('Enter Citizentship')
+// if (age >= 18 && citizenship==='South African') 
+// { 
+//     alert('Hi ' + name + ', you can vote')
+// }
 
-else{
-    alert('Hi ' + name + ", you can't vote")
-}
+// else{
+//     alert('Hi ' + name + ", you can't vote")
+// }
 
