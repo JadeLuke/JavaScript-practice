59c4d83176a9eafd79c4ae86918ac00b50242d49
JadeLuke <jadeluke.booysen@gmail.com>
2024-06-03 10:36:55 +0200

    Added voting system

---
+++ main.js
@@ -0,0 +1,12 @@
+let age = prompt('Enter your age');
+let citizenship = prompt('Enter Citizentship')
+if (age >= 18 && citizenship==='South African') 
+{ 
+    console.log('Can vote')
+    alert('You can vote')
+}
+
+else{
+    alert("You can't vote")
+}
+
